9d0d94de0681fc8b4f142dd3f7fe36c99f3951d5
kyriacos kousoulides <gli_koullis@msn.com>
2014-07-23 15:17:08 +0300

    Update ofSystemUtils.h
    isolated the ofTextfield class - removed irrelevant ofsystemutils code

--- ofxTextField/ofSystemUtils.h
+++ ofxTextField/ofSystemUtils.h
@@ -1,28 +1,7 @@
-#pragma once
-
-
-class ofFileDialogResult{
-	public:
-		ofFileDialogResult();
-		
-		//TODO: only 1 file for now
-		string getName();
-		string getPath();
-		
-		string filePath;
-		string fileName;
-		bool bSuccess;
-};
-
-void ofSystemAlertDialog(string errorMessage);
-ofFileDialogResult ofSystemLoadDialog(string windowTitle="", bool bFolderSelection = false, string defaultPath="");
-ofFileDialogResult ofSystemSaveDialog(string defaultName, string messageName);
-string ofSystemTextBoxDialog(string question, string text="");
-
 
 ///--------------------------------------------------------------------------
 ///--------------------------------------------------------------------------
-///TEXTBOX FOR OF
+///TEXTBOX FOR OF add the following code inside ofSystemUtils.h
 ///--------------------------------------------------------------------------
 ///--------------------------------------------------------------------------
 
